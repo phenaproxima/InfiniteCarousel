a24afd0f908d34e41db98270591c599a5384570d
Phéna Proxima <djphenaproxima@gmail.com>
2014-12-10 21:01:09 -0500

    The carousel can now accept options overriding the image renderer and options for animation.

--- Backbone.InfiniteCarousel.js
+++ Backbone.InfiniteCarousel.js
@@ -1,32 +1,36 @@
 (function($, _, Backbone) {
 
-var ImageView = Backbone.View.extend({
-
-  tagName: 'figure',
-
-  render: function() {
-    $('<img />').attr('src', this.model.get('image')).appendTo(this.el);
-  }
-
-});
-
 Backbone.InfiniteCarousel = Backbone.View.extend({
 
-  initialize: function() {
+  imageView: Backbone.View.extend({
+
+    tagName: 'figure',
+
+    render: function() {
+      $('<img />').attr('src', this.model.get('image')).appendTo(this.el);
+    }
+
+  }),
+
+  animation: {},
+
+  initialize: function(options) {
+    _.extend(this, _.pick(options, 'imageView', 'animation'));
+
     this._prevButton = $('<button></button>').html('&laquo;').attr({ rel: 'prev', disabled: true }).on('click', { controller: this }, this._onPrevClick);
     this._nextButton = $('<button></button>').html('&raquo;').attr({ rel: 'next', disabled: true }).on('click', { controller: this }, this._onNextClick);
     this._track = $('<div></div>').width('400%');
 
-    $('<div></div>').width('25%').append(this.collection.getPage().map(this._renderImage)).appendTo(this._track);
+    $('<div></div>').width('25%').append(this.collection.getPage().map(this._renderImage, this)).appendTo(this._track);
 
     var controller = this;
 
-    $('<div></div>').width('25%').append(this.collection.behind().map(this._renderImage)).prependTo(this._track).imagesLoaded(function() {
+    $('<div></div>').width('25%').append(this.collection.behind().map(this._renderImage, this)).prependTo(this._track).imagesLoaded(function() {
       controller._prevButton.removeAttr('disabled');
       controller._track.css('margin-left', '-100%');
     });
     
-    $('<div></div>').width('25%').append(this.collection.ahead().map(this._renderImage)).appendTo(this._track).imagesLoaded(function() {
+    $('<div></div>').width('25%').append(this.collection.ahead().map(this._renderImage, this)).appendTo(this._track).imagesLoaded(function() {
       controller._nextButton.removeAttr('disabled');
     });
 
@@ -34,7 +38,7 @@ Backbone.InfiniteCarousel = Backbone.View.extend({
   },
 
   _renderImage: function(i) {
-    var v = new ImageView({ model: i });
+    var v = new this.imageView({ model: i });
     v.render();
     return v.el;
   },
@@ -42,27 +46,31 @@ Backbone.InfiniteCarousel = Backbone.View.extend({
   _onPrevClick: function(event) {
     var controller = event.data.controller;
 
-    controller.collection.retreat();
-    controller._prevButton.attr('disabled', true);
-    controller._track.animate({ marginLeft: '+=100%' }, function() {
-      $('<div></div>').width('25%').append(controller.collection.behind().map(controller._renderImage)).imagesLoaded(function() {
+    function after() {
+      $('<div></div>').width('25%').append(controller.collection.behind().map(controller._renderImage, controller)).imagesLoaded(function() {
         $(this.elements[0]).prependTo(controller._track).parent().css('margin-left', '-100%').children().last().remove();
         controller._prevButton.removeAttr('disabled');
       });
-    });
+    }
+
+    controller.collection.retreat();
+    controller._prevButton.attr('disabled', true);
+    controller._track.animate({ marginLeft: '+=100%' }, _.extend({}, controller.animation, { complete: after }));
   },
 
   _onNextClick: function(event) {
     var controller = event.data.controller;
 
-    controller.collection.advance();
-    controller._nextButton.attr('disabled', true);
-    controller._track.animate({ marginLeft: '-=100%' }, function() {
-      $('<div></div>').width('25%').append(controller.collection.ahead().map(controller._renderImage)).imagesLoaded(function() {
+    function after() {
+      $('<div></div>').width('25%').append(controller.collection.ahead().map(controller._renderImage, controller)).imagesLoaded(function() {
         $(this.elements[0]).appendTo(controller._track).parent().css('margin-left', '-100%').children().first().remove();
         controller._nextButton.removeAttr('disabled');
       });
-    });
+    }
+
+    controller.collection.advance();
+    controller._nextButton.attr('disabled', true);
+    controller._track.animate({ marginLeft: '-=100%' }, _.extend({}, controller.animation, { complete: after }));
   },
 
   render: function() {
